0eaaaf492e3a9cd1e3c717e5d7693a1088970aaa
ProSweeper <dylan.doucet28@gmail.com>
2022-12-15 23:15:38 +0000

    implement responsive design for exercise index page

--- src/components/ExerciseListItem/ExerciseListItem.jsx
+++ src/components/ExerciseListItem/ExerciseListItem.jsx
@@ -1,9 +1,26 @@
+import './ExerciseListItem.css';
+
 export default function ExerciseListItem({exercise}) {
+  function capitalize(string) {
+    const arr = string.split(' ');
+    const newArr = arr.map((word) => word = word.charAt(0).toUpperCase() + word.slice(1));
+    return newArr.join(' ');
+  }
+
   return (
-    <>
-      <h3>{exercise.name}</h3>
-      <p>Equipment: {exercise.equipment}</p>
-      <p>Target Muscle: {exercise.target}</p>
-    </>
+    <div className='exerciseListItem'>
+      <div className='info'>
+        <h3>{capitalize(exercise.name)}</h3>
+        <p><span>Equipment:</span> {capitalize(exercise.equipment)}</p>
+        <p><span>Target Muscle:</span> {capitalize(exercise.target)}</p>
+      </div>
+      <div className='gif'>
+        <img src={exercise.gifUrl} alt="demo gif" width={'100%'} />
+      </div>
+      <div className='buttons'>
+        <div className="button detail" data-alttext="D"><span>Detail</span></div>
+        <div className="button add" data-alttext="A"><span>Add</span></div>
+      </div>
+    </div>
   );
 }
